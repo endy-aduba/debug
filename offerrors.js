9b45e4211b49e1414345c733b34a1a2fd9228ca7
endy <endyaduba@gmail.com>
2019-08-29 22:48:04 +0100

    final addition

---
+++ offerrors.js
@@ -0,0 +1,5 @@
+// Debugging: Catch Off By One Errors When Using Indexing
+// Off by one errors (sometimes called OBOE) crop up when you're trying to target a specific index of a string or array (to slice or access a segment), or when looping over the indices of them. JavaScript indexing starts at zero, not one, which means the last index is always one less than the length of the item. If you try to access an index equal to the length, the program may throw an "index out of range" reference error or print undefined.
+
+// When you use string or array methods that take index ranges as arguments, it helps to read the documentation and understand if they are inclusive (the item at the given index is part of what's returned) or not. Here are some examples of off by one errors:
+
